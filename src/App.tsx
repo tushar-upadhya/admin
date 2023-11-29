e53e8d59db250e032a4cd0ec46e7874ee01f3680
tushar-upadhya <tusharupadhyay691@gmail.com>
2023-11-29 19:07:19 +0530

    routes and scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Suspense, lazy } from "react";
+
+import Loader from "./components/Loader";
+
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Products = lazy(() => import("./pages/Products"));
+const Transactions = lazy(() => import("./pages/Transactions"));
+const Customers = lazy(() => import("./pages/Customers"));
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/product" element={<Products />} />
+          <Route path="/admin/transaction" element={<Transactions />} />
+          <Route path="/admin/customer" element={<Customers />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import Sidebar from "../components/Sidebar";
+
+const Dashboard = () => {
+  return (
+    <div className="adminContainer">
+      <Sidebar />
+      <main></main>
+    </div>
+  );
+};
+
+export default Dashboard;
