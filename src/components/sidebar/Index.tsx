c05969e22e76fe998afc8299e828ce95b83a8e40
tushar-upadhya <tusharupadhyay691@gmail.com>
2023-12-07 08:58:35 +0530

    media query

--- src/components/sidebar/Index.tsx
+++ src/components/sidebar/Index.tsx
@@ -2,15 +2,63 @@ import { useLocation } from "react-router-dom";
 import Apps from "./Apps";
 import Charts from "./Charts";
 import Sidebar from "./Sidebar";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { useEffect, useState } from "react";
 
 const Index = () => {
   const location = useLocation();
+
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [phoneActive, setPhoneActive] = useState<boolean>(
+    window.innerWidth < 1100
+  );
+
+  const resizeHandler = () => {
+    setPhoneActive(window.innerWidth < 1100);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", resizeHandler);
+
+    return () => {
+      window.removeEventListener("resize", resizeHandler);
+    };
+  }, []);
+
   return (
-    <aside>
-      <Sidebar location={location} />
-      <Charts location={location} />
-      <Apps location={location} />
-    </aside>
+    <>
+      {phoneActive && (
+        <button id="hamburger" onClick={() => setShowModal(true)}>
+          <HiMenuAlt4 />
+        </button>
+      )}
+
+      <aside
+        style={
+          phoneActive
+            ? {
+                width: "20rem",
+                height: "100vh",
+                position: "fixed",
+                top: 0,
+                left: showModal ? "0" : "-20rem",
+                transition: "all 0.5s",
+              }
+            : {}
+        }
+      >
+        <h2>Logo.</h2>
+        <Sidebar location={location} />
+        <Charts location={location} />
+        <Apps location={location} />
+
+        {phoneActive && (
+          <button id="close_sidebar" onClick={() => setShowModal(false)}>
+            Close
+          </button>
+        )}
+      </aside>
+    </>
   );
 };
 
